daaabf10beb0f4d196b90bbc6edf6df6f26b89ef
fko22 <83680488+fko22@users.noreply.github.com>
2024-12-01 10:19:19 -0800

    attempt 3

--- utils.py
+++ utils.py
@@ -60,10 +60,8 @@ def load_api_key():
             os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
     # Use the API key
-    print("""st.secrets["OPENAI_API_KEY"]: """, st.secrets["OPENAI_API_KEY"])
-    print("""st.secrets["OPENAI_API_KEY"]["OPENAI_API_KEY"]: """, st.secrets["OPENAI_API_KEY"]["OPENAI_API_KEY"])
-    api_key = st.secrets["OPENAI_API_KEY"]
-    print(api_key)
+    api_key = os.environ["OPENAI_API_KEY"]
+    print("api_key",api_key)
 
     if not api_key:
         raise ValueError("API key not found. Please set it in .env or Streamlit secrets.")
